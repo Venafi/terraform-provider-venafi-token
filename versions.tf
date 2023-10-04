67c0cefb2d0cf2c02dfdc21aee3915b27be294d0
Russel Vela <russel.vela@venafi.com>
2023-10-04 10:25:09 -0600

    fix(logs): Adds log to successful resource deletion

--- versions.tf
+++ versions.tf
@@ -5,11 +5,11 @@ terraform {
     }
     venafi-token = {
       source  = "Venafi/venafi-token"
-      version = "0.1.0"
+#      version = "0.1.0"
     }
     venafi = {
       source  = "Venafi/venafi"
-      version = "0.17.0"
+      version = "0.17.1"
     }
   }
 }
